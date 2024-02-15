af1e66e4e7d32eb2e5fb9c9f720925159a97a0c8
zz41d <zaidali9347@gmail.com>
2024-02-15 19:01:17 +0530

    first commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,14 @@
+<template>
+    <main class="container">
+        <h1>About</h1>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita, deserunt nisi! A pariatur iusto maxime veniam similique nisi! Voluptas provident, ea reprehenderit placeat id et necessitatibus sequi magnam cupiditate in.
+        Debitis harum magni obcaecati voluptate ad aperiam corporis rem voluptatem. Itaque, cum dolorum. Recusandae soluta temporibus debitis dolores magnam eveniet vel sapiente perferendis accusamus, est consequatur laboriosam laborum eligendi sunt?
+        Nisi magni assumenda sed laboriosam aperiam sequi dolorum, nobis ducimus odio consectetur necessitatibus expedita qui, iure a odit, totam officiis alias optio amet vel. Dolore earum repellendus dolores consequuntur eius.
+        </p>
+    </main>
+</template>
+<style scoped>
+p{
+    text-align: center;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,42 @@
+<script setup>
+import phoneData from '../data.json'
+import {ref} from 'vue'
+
+const phones=ref(phoneData)
+</script>
+
+<template>
+  <main class="container">
+ 
+    <h1>Our Phones</h1>
+    <div class="cards">
+      <div v-for="phone in phones" :key="phone.id"   class="card">
+        <h1>{{ phone.make }}</h1>
+        <p>₹{{ phone.price }}</p>
+      </div>
+    </div>
+  </main>
+</template>
+
+
+<style scoped>
+  .cards {
+  display: flex;
+  width: 1000px;
+  flex-wrap: wrap;
+  margin-top: 50px;
+  justify-content: center;
+}
+
+.card {
+  box-shadow: 1px 1px 10px rgba(0, 0, 0, 0.207);
+  padding: 15px;
+  width: 150px;
+  margin-right: 15px;
+  cursor: pointer;
+  margin-bottom: 20px;
+
+}
+
+
+</style>
